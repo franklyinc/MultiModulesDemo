c763b1fc6ca7330c0e1baf4040399124656d2f2f
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-02-02 14:14:00 -0500

    Update index.js

--- mod1/index.js
+++ mod1/index.js
@@ -1,9 +1,7 @@
 import React asdf, { Component, PropTypes } from 'react';
 
 class MultiModulesDemo1 extends Component {
-	asdf
   render(){
-	  asdf
     return (
       <div className='FranklyMultiModulesDemo'>
 	      <h2 className='demo_text'>mod1: {this.props.text}</h2>
