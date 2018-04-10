724453571cd7862aa424df5a91744006b95efdd4
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-10 17:57:11 -0400

    Update index.js

--- mod1/index.js
+++ mod1/index.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
+import Shared1 from '../shared1';
 
 class MultiModulesDemo1 extends Component {
   render(){
     return (
       <div className='FranklyMultiModulesDemo'>
 	      <h2 className='demo_text'>mod1: {this.props.text}</h2>
+	      <Shared1></Shared1>
       </div>
     );
 	  
