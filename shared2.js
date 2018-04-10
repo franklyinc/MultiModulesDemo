7d43029c06106a9853557139a1130c5c4817e7d2
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-10 17:56:31 -0400

    Create shared2.js

---
+++ shared2.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class Shared2 extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='Shared2'>
+	      Shared2</br>
+      </div>
+    );
+	  
+  }
+}
+
+export default Shared2;
