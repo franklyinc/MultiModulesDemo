de5c4864061b02ec518f3cdaa55bf644fbf2c6da
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-10 20:41:02 -0400

    Update shared1.js

--- shared1.js
+++ shared1.js
@@ -1,18 +1,24 @@
-import React, { Component, PropTypes } from 'react';
-import shared2 from './shared2';
+// import React, { Component, PropTypes } from 'react';
+// import shared2 from './shared2';
 
-class shared1 extends Component {
+// class shared1 extends Component {
 	
-  render(){
+//   render(){
 	  
-    return (
-      <div className='shared1'>
-	shared1<br />
-        <shared2></shared2>
-      </div>
-    );
+//     return (
+//       <div className='shared1'>
+// 	shared1<br />
+//         <shared2></shared2>
+//       </div>
+//     );
 	  
-  }
+//   }
+// }
+
+// export default shared1;
+
+function shared1 (n) {
+  return n * 2
 }
 
-export default shared1;
+export default shared1
